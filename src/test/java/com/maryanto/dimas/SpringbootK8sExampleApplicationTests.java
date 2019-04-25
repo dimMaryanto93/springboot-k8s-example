0f8ac2255dd29b0d29ba17502acabfd74b66677c
dimmaryanto93 <software.dimas_m93@icloud.com>
2019-04-25 11:09:38 +0700

    init project springboot

---
+++ src/test/java/com/maryanto/dimas/SpringbootK8sExampleApplicationTests.java
@@ -0,0 +1,16 @@
+package com.maryanto.dimas;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootK8sExampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
